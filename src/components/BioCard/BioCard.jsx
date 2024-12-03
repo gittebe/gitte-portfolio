fc64d04afb15301a52e5d5a15c21de1d44aa51ce
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-12-03 14:17:47 +0100

    - changed text in bio section

--- src/components/BioCard/BioCard.jsx
+++ src/components/BioCard/BioCard.jsx
@@ -14,7 +14,7 @@ export const BioCard = () => {
         </HeaderOne>
         <div className="spacer-one"></div>
         <Body>
-          I am a junior web developer with a strong background in problem-solving and creating user-friendly applications. With a diverse international professional journey, including teaching, research, and project management, I bring a unique perspective to my work. I am passionate about developing intuitive web solutions that prioritize functionality and user experience.
+          I am a web developer with a strong background in problem-solving and creating user-friendly applications. With a diverse international professional journey, including teaching, research, and project management, I bring a unique perspective to my work. I am passionate about developing intuitive web solutions that prioritize functionality and user experience.
         </Body>
         <div className="spacer-two"></div>
         <MediaButtonContainer />
