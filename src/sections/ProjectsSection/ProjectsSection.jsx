a619a662ee6ce8c0ca61aafca06da1905d11ed0a
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-02-08 12:21:51 +0100

    - added buttons to scroll right and left in projects for bigger screens

--- src/sections/ProjectsSection/ProjectsSection.jsx
+++ src/sections/ProjectsSection/ProjectsSection.jsx
@@ -1,18 +1,45 @@
+import { useState, useRef } from "react";
 import { HeaderTwo } from "../../ui/Typography/Typography";
 import { ProjectCard } from "../../components/ProjectCard/ProjectCard";
 import "./ProjectsSection.css"
+import Slider from "react-slick";
+import { ScrollButtonLeft } from "../../ui/ScrollButtonLeft/ScrollButtonLeft";
+import { ScrollButtonRight } from "../../ui/ScrollButtonRight/ScrollButtonRight";
 
 export const ProjectsSection = () => {
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const containerRef = useRef(null);
+
+  const scroll = (direction) => {
+    if (containerRef.current) {
+      const scrollAmount = direction === "left" ? -300 : 300;
+      containerRef.current.scrollBy({left: scrollAmount, behavior: "smooth"})
+    }
+  }
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <section className="projects-section">
       <HeaderTwo>
         Featured Projects
       </HeaderTwo>
-      <div className="projects-scroll-container">
-        <ProjectCard></ProjectCard>
+      <div className="project-section-container">
+        <ScrollButtonLeft scroll={scroll}/>
+        <div className="projects-scroll-container" ref={containerRef}>
+          <ProjectCard></ProjectCard>
+        </div>
+        <ScrollButtonRight scroll={scroll}/>
+      </div>
+
+      <div className="projects-scroll-container-mobile">
+          <ProjectCard></ProjectCard>
       </div>
     </section>
-
   )
 }
-
