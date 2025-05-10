ea52fce15c853b9cb12cbad76c44094b3cc885f9
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-05-10 22:58:35 +0200

    - added autofill and changed font color

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -35,6 +35,7 @@ export const ContactForm = () => {
             name="bot-field"
             value={formData.botField}
             onChange={handleChange}
+            autoComplete="off"
           />
         </label>
       </p>
