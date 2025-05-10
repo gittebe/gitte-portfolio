bbeef75f8aa4f4d6308c1589361ee81d7501ca94
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-05-10 12:25:23 +0200

    - added <noscript> to body

--- src/components/BioCard/BioCard.jsx
+++ src/components/BioCard/BioCard.jsx
@@ -12,16 +12,14 @@ export const BioCard = () => {
         <HeaderOne>
           Web Developer
         </HeaderOne>
-        <div className="spacer-one"></div>
-        <Body>
+        <section className="bio-text-section">
+        <Body className="bio-text paragraph-spacing">
           Hello! I'm Gitte, a full-stack web developer with international work experience across Europe, East Africa, and the United States. I now live in a small village in Sörmland, about an hour by train from Stockholm, Sweden.
-        </Body>
-        <Body>
-          My first introduction to HTML was back in 2006. Since then, content management and website development with CMS platforms have been part of my professional and volunteer work. However, it wasn't until 2024 that I decided to fully pursue web development as my full-time profession.
         </Body>
         <Body>
           What fascinates me about coding is seeing ideas come to life, being able to learn continuously, solve problems, and find creative ways to make things happen. Right now, my biggest passion is working with React: building complex structures, designing user interfaces, and connecting the frontend to APIs to display data in a way that’s both accessible and engaging for users.
         </Body>
+        </section>
         <div className="spacer-two"></div>
         <MediaLinkContainer />
       </div >
