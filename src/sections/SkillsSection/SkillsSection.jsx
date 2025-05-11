4f0ec40a1747462497c09ceee263d79a8246d300
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-05-11 10:13:10 +0200

    - changed the skills section

--- src/sections/SkillsSection/SkillsSection.jsx
+++ src/sections/SkillsSection/SkillsSection.jsx
@@ -1,33 +1,64 @@
-import { SkillsCard } from "../../components/SkillsCard/SkillsCard";
-import { Grid } from "../../ui/Grid/Grid";
 import { HeaderTwo } from "../../ui/Typography/Typography";
 import "./SkillsSection.css"
 
 export const SkillsSection = () => {
   return (
-    <Grid background="neutral">
-      <section className="skills-section"
-      id="skills"
-      >
-        <HeaderTwo>
-          Skills
-        </HeaderTwo>
-        <div className="spacer-one"></div>
-        <div className="skills-container">
-          <SkillsCard
-            header="Languages"
-            body={["HTML5", "CSS3", "JavaScript", "TypeScript", "Java", "SQL"]} />
-          <SkillsCard
-            header="Tech"
-            body={["React", "Global State Management", "DOM Manipulation", "Responsive Design", "Styled Components", "Zustand", "Node.js", "Express.js", "MongoDB", "Mongoose"]} />
-          <SkillsCard
-            header="Dev Tools"
-            body={["Figma", "Github", "VS Code", "Chrome dev tools", "Postman", "Accessibility"]} />
-          <SkillsCard
-            header="More"
-            body={["Project Management", "Qualitative Research", "German", "English", "Swedish"]} />
+    <section className="skills-section">
+      <HeaderTwo>Skills</HeaderTwo>
+      <article className="skills">
+        <div>
+          <img src="../assets/logo-accessibility.png" alt=""></img>
+          <p>Accessibility</p>
         </div>
-      </section>
-    </Grid>
+        <div>
+          <img src="/assets/logo-github.png" alt=""></img>
+          <p>gitHub</p>
+        </div>
+        <div>
+          <img src="/assets/logo-html.png" alt=""></img>
+          <p>HTML</p>
+        </div>
+        <div>
+          <img src="/assets/logo-css.png" alt=""></img>
+          <p>CSS</p>
+        </div>
+        <div>
+          <img src="/assets/logo-java-script.png" alt=""></img>
+          <p>JavaScript</p>
+        </div>
+        <div>
+          <img src="/assets/logo-java.png" alt=""></img>
+          <p>Java</p>
+        </div>
+         <div>
+          <img src="/assets/logo-react.png" alt=""></img>
+          <p>React</p>
+        </div>
+         <div>
+          <img src="/assets/logo-express.png" alt=""></img>
+          <p>Express.js</p>
+        </div>
+        <div>
+          <img src="/assets/logo-mongodb.png" alt=""></img>
+          <p>MongoDB</p>
+        </div>
+        <div>
+          <img src="/assets/logo-responsive.png" alt=""></img>
+          <p>Responsive</p>
+        </div>
+        <div>
+          <img src="/assets/logo-node.png" alt=""></img>
+          <p>Node.js</p>
+        </div>
+        <div>
+          <img src="/assets/logo-sql.png" alt=""></img>
+          <p>SQL</p>
+        </div>
+        <div>
+          <img src="/assets/logo-figma.png" alt=""></img>
+          <p>Figma</p>
+        </div>
+      </article>
+    </section>
   )
 }
