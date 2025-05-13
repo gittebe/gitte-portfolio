4f44ce706d5aa15009581e208d99519abb2c8140
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-05-13 08:18:22 +0200

    - added a ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Home } from "./pages/Home";
 import { Article } from "./pages/Article";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {ScrollToTop} from "./ui/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
