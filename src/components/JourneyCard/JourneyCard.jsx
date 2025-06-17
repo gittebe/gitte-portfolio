79ec87b62dec9525590db66974f43e585c6b5242
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-06-17 10:25:37 +0200

    - changed link design and added prop types

--- src/components/JourneyCard/JourneyCard.jsx
+++ src/components/JourneyCard/JourneyCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { Body, HeaderThree } from "../../ui/Typography/Typography";
 import { CardImage } from "../../ui/CardImage/CardImage";
 import "./JourneyCard.css";
-import { useNavigate } from "react-router-dom";
+import PropTypes from 'prop-types';
 
 const truncateText = (text, length) => {
   const tempText = text.replace(/<\/?[^>]+(>|$)/g, "");
@@ -13,7 +13,7 @@ export const JourneyCard = ({ journey, index }) => {
   if (!journey) {
     return <div className="journey-card">No journey data available.</div>;
   }
-  const navigate = useNavigate();
+
   return (
     <div className="journey-card">
       <CardImage cardImage={journey.cardImage} alt={journey.alt} />
@@ -34,4 +34,14 @@ export const JourneyCard = ({ journey, index }) => {
       </Link>
     </div>
   );
+};
+
+JourneyCard.propTypes = {
+  journey: PropTypes.shape({
+    cardImage: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    header: PropTypes.string.isRequired,
+    article: PropTypes.string.isRequired,
+  }),
+  index: PropTypes.number.isRequired,
 };
