3393236ff51749bd7c65679309b0f34944a015ca
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-06-19 21:33:19 +0200

    / changed text in BioCard.jsx to improve clarity and readability

--- src/components/BioCard/BioCard.jsx
+++ src/components/BioCard/BioCard.jsx
@@ -11,7 +11,7 @@ export const BioCard = () => {
         <p className="web-developer">Web Developer</p>
         <section className="bio-text-section">
         <Body>
-          I'm a web developer and CPACC-certified accessibility specialist with a strong academic background in research and education. I bring a unique perspective to web development through my interdisciplinary experience in anthropology, disability studies, and inclusive design. After completing an intensive web development bootcamp at Technigo in Stockholm, I have been actively involved in building accessible and user-friendly web applications using technologies like HTML, CSS, JavaScript, TypeScript, React, and Node.js. My current pro-bono work focuses on creating inclusive websites for small organizations and community initiatives, aligning with my commitment to digital equity and universal design.
+          I'm a web developer and CPACC-certified accessibility specialist with a strong academic background in research and education.
         </Body>
         <Body>
           Before transitioning into tech, I spent over a decade in academia and education, working internationally as a researcher, lecturer, and language instructor. My doctoral work explored accessibility and inclusion in post-conflict Uganda, and I’ve led international research collaborations, taught in multicultural classrooms, and curated exhibitions. 
